bea5c61a804049e997d3f93f6ab8eacac766793b
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-11 11:05:51 -0300

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Maia Fattori Abati on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
