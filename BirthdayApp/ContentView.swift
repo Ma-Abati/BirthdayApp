2648a7d56734720d7cec5694cb6490199c579f95
Maia Fattori Abati <maiaabati@gmail.com>
2025-07-11 12:35:22 -0300

    Set up UI, first step of the code(stage 3)

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,16 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    //friend array
+    @State private var friends: [Friend] = [
+        Friend(name: "Maia", birthday: .now),
+        Friend(name: "Aira", birthday: .now)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        NavigationStack{
+            
+            List(friends, id: \.name){ friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text ("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date){
+                        TextField("Name", text: $newName )
+                            .textFieldStyle(.roundedBorder)
+                           
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                          
+                    }
+                
+                    .bold()
+                }
+                .padding()
+            }
+            
+        }  // nav stack
+        
+    } //closing body
 }
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
---
+++ BirthdayApp/Friends.swift
@@ -0,0 +1,19 @@
+//
+//  Friends.swift
+//  BirthdayApp
+//
+//  Created by Maia Fattori Abati on 11/07/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday 
+    }
+}
+
